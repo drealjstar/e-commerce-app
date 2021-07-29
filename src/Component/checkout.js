2b593cb941dff9fa02e19a2f9b0529a79569cde6
jstar <drealjstar@yahoo.com>
2021-07-28 23:54:08 -1200

    code adjustment

--- src/Component/checkout.js
+++ src/Component/checkout.js
@@ -1,58 +1,58 @@
-import React, {Component} from "react";
-import {ToastsContainer, ToastsStore, ToastsContainerPosition} from "react-toasts";
+import React, { Component } from "react";
+import { ToastsContainer, ToastsStore, ToastsContainerPosition } from "react-toasts";
 import StripeCheckout from "react-stripe-checkout";
 import './checkout.css';
-import {Link} from 'react-router-dom';
-import {getTotal} from './add';
+import { Link } from 'react-router-dom';
+import { getTotal } from './add';
 import userContext from '../Context/userContext';
 
 
 class Checkout extends Component {
-    constructor(prop){
-       super(prop);
-        this.thanks = this.thanks.bind(this);
-   }
-   thanks(token){
-     console.log(token)
-     return ToastsStore.success('Payment was successful')
-   }
-  
-    render() {
-      
-      return(
-        <userContext.Consumer>
-        {props =>{
-     
-         const {cart}= props.userDetails;
-     
-         const total= getTotal(cart);
-     
-        return (
-          <div className='paymentBox'>
-           <Link to="/total_amount" className='back'>
-           <p className='back_text3'>Back</p>
-           </Link>
-           
-           <div className='stripePayment'>
+  constructor(prop) {
+    super(prop);
+    this.thanks = this.thanks.bind(this);
+  }
+  thanks(token) {
+    console.log(token)
+    return ToastsStore.success('Payment was successful')
+  }
+
+  render() {
+
+    return (
+      <userContext.Consumer>
+        {props => {
+
+          const { cart } = props.userDetails;
+
+          const total = getTotal(cart);
+
+          return (
+            <div className='paymentBox'>
+              <Link to="/total_amount" className='back'>
+                <p className='back_text3'>Back</p>
+              </Link>
+
+              <div className='stripePayment'>
                 <h2 className='heading'>Make your payment with <span>stripe</span> </h2>
                 <StripeCheckout
-                 stripeKey="pk_test_51JB0aTHScwpLZvovonxaYlv150xIhFAgXVe8YFLOKBwjs4H3LRdqIg9TCt5jP6eVpt4Cl4bwRWUsvIwzZ2rB2nrz00PBFJHXk5"
-                 name='Payment of item'
-                 description='BUYTIME PRODUCT'
-                 amount= {total * 100}
-                 token={this.thanks}
-                 currency='USD'
-                 />
-                 <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.TOP_RIGHT}/>
+                  stripeKey="pk_test_51JB0aTHScwpLZvovonxaYlv150xIhFAgXVe8YFLOKBwjs4H3LRdqIg9TCt5jP6eVpt4Cl4bwRWUsvIwzZ2rB2nrz00PBFJHXk5"
+                  name='Payment of item'
+                  description='BUYTIME PRODUCT'
+                  amount={total * 100}
+                  token={this.thanks}
+                  currency='USD'
+                />
+                <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.TOP_RIGHT} />
               </div>
               <div className='image'>
               </div>
             </div>
-        )
-    }
-}
-</userContext.Consumer>
-   );
+          )
+        }
+        }
+      </userContext.Consumer>
+    );
   }
 }
 
--- src/Component/full_info.js
+++ src/Component/full_info.js
@@ -1,62 +1,62 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './full_info.css';
 
-class FullInfo extends Component{
-state = {
-quantity : 1
+class FullInfo extends Component {
+  state = {
+    quantity: 1
 
-}
+  }
 
-decrementQuantity = () =>{
-  if(this.state.quantity < 1){
+  decrementQuantity = () => {
+    if (this.state.quantity < 1) {
       this.setState({
-        quantity : 1
+        quantity: 1
       });
-    }else{
-    this.setState({
-      quantity : this.state.quantity - 1,
-    });
-  }
+    } else {
+      this.setState({
+        quantity: this.state.quantity - 1,
+      });
+    }
   }
 
-  incrementQuantity = () =>{
+  incrementQuantity = () => {
     this.setState({
-      quantity : this.state.quantity + 1,
+      quantity: this.state.quantity + 1,
     })
   }
 
-   render(){
+  render() {
 
-    const {location, addToCart} = this.props;
-    const {productData} = location;
-    const {quantity}= this.state;
-   
-    return(
-     <div className= 'full_info'>
-       <Link to="/" className='back'>
-       <p className='back_text'>Back</p>
-      </Link>
-      {productData ? (
-      <div className= 'info_card'>
-      <img src={productData.image}  alt= "no"  className= 'info_img'/>
+    const { location, addToCart } = this.props;
+    const { productData } = location;
+    const { quantity } = this.state;
 
-      <div className= 'info'>
-      <p className='info_title'>{productData.title}</p>
-      <h1 className='info_price'>{productData.price}</h1>
-           
-      <div className= 'quantities-button'>
-      <button className= 'decrease-button'  onClick={this.decrementQuantity}>−</button>
-      <button className='quantity-button'>{this.state.quantity}</button>
-      <button className= 'increase-button' onClick={this.incrementQuantity}>+</button>
+    return (
+      <div className='full_info'>
+        <Link to="/" className='back'>
+          <p className='back_text'>Back</p>
+        </Link>
+        {productData ? (
+          <div className='info_card'>
+            <img src={productData.image} alt="no" className='info_img' />
+
+            <div className='info'>
+              <p className='info_title'>{productData.title}</p>
+              <h1 className='info_price'>{productData.price}</h1>
+
+              <div className='quantities-button'>
+                <button className='decrease-button' onClick={this.decrementQuantity}>−</button>
+                <button className='quantity-button'>{this.state.quantity}</button>
+                <button className='increase-button' onClick={this.incrementQuantity}>+</button>
+              </div>
+              <button className='info_button' onClick={() => addToCart({ ...productData, quantity })}>ADD TO CART</button>
+            </div>
+          </div>
+        ) : <p className='no-data'> no data available </p>}
       </div>
-      <button className= 'info_button' onClick={()=> addToCart({...productData, quantity})}>ADD TO CART</button>
-      </div>
-      </div> 
-        ) : <p className= 'no-data'> no data available </p>}
-      </div>
-     );
-   }
- }
+    );
+  }
+}
 
 export default FullInfo;
